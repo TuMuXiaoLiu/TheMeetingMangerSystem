698de55bac2ebb14f4a035d161183beb20c0ed0e
TuMuXiaoLiu <chunlincao@foxmail.com>
2017-05-23 17:36:55 +0800

    add Department

---
+++ src/fff/ccl/mettingmanger/pojo/Department.java
@@ -0,0 +1,27 @@
+package fff.ccl.mettingmanger.pojo;
+
+/**
+ * @author ChulinCao E-mail: caochunlin@chinasofti.com
+ * @date 2017年5月23日 下午12:21:18
+ * @version 1.8
+ * @since
+ * @parameter
+ * @PS
+ */
+public class Department {
+	/**
+	 * 部门名称 唯一
+	 */
+	private String departmentName;
+
+	/**
+	 * 部门编号 唯一
+	 */
+	private int departmentId;
+
+	/**
+	 * 备注
+	 */
+	private String remark;
+
+}
