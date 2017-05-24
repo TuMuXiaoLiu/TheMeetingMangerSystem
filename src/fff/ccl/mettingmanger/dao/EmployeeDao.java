a6297086d6464a8823211447978560e6b104a324
zrgjui <zrgjui@ccl>
2017-05-24 11:04:12 +0800

    add interface Dao and add class EmployeeDao

---
+++ src/fff/ccl/mettingmanger/dao/EmployeeDao.java
@@ -0,0 +1,36 @@
+package fff.ccl.mettingmanger.dao;
+
+/**
+ * @author caochunlin E-mail: caochunlin@chinasofti.com
+ * @date 创建时间：2017年5月24日 上午11:02:14
+ * @version 1.8
+ * @since
+ * @PS 员工类的增删改查
+ */
+public class EmployeeDao implements Dao {
+
+	@Override
+	public boolean insert(Object object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Object object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object selectById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object selectByIdPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
