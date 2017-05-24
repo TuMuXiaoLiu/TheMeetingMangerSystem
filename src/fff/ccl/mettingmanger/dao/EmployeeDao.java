ec48b3df843a7eb87373657c3cb49f83f587465d
TuMuXiaoLiu <chunlincao@foxmail.com>
2017-05-24 18:09:06 +0800

    EmployeeDao1.0 --> ok

--- src/fff/ccl/mettingmanger/dao/EmployeeDao.java
+++ src/fff/ccl/mettingmanger/dao/EmployeeDao.java
@@ -44,18 +44,26 @@ public class EmployeeDao {
 		Connection connection = ConnectionFactoryOracle.getConnection();
 		PreparedStatement ps = null;
 		try {
+			Employee employee = selectById(e.getEmployeeId());
+			/*
+			 * // 判断修改的是否是密码
+			 * if(e.getUserPassword().equals(employee.getUserPassword())) {
+			 * 
+			 * }
+			 */
+
 			ps = connection.prepareStatement(
-					"update  employee set username =? values(?,?,?,?,?,?,?,?,?) where employeeid = ?");
+					"update  employee set employeeName =?,username=?, userpassword = ?, phone = ?, email = ?, deptid = ?, roleid = ?, employeestatus = ?, remark = ? where employeeid = ?");
 			ps.setInt(10, e.getEmployeeId());
-			ps.setString(2, e.getEmployeeName());
-			ps.setString(3, e.getUserName());
-			ps.setString(4, e.getUserPassword());
-			ps.setLong(5, e.getPhone());
-			ps.setString(6, e.getEmail());
-			ps.setInt(7, e.getDeptid());
-			ps.setInt(8, e.getRoleId());
-			ps.setInt(9, e.getEmployeeStatus());
-			ps.setString(10, e.getRemark());
+			ps.setString(1, e.getEmployeeName());
+			ps.setString(2, e.getUserName());
+			ps.setString(3, e.getUserPassword());
+			ps.setLong(4, e.getPhone());
+			ps.setString(5, e.getEmail());
+			ps.setInt(6, e.getDeptid());
+			ps.setInt(7, e.getRoleId());
+			ps.setInt(8, e.getEmployeeStatus());
+			ps.setString(9, e.getRemark());
 			ps.execute();
 			return true;
 		} catch (SQLException e1) {
@@ -64,12 +72,12 @@ public class EmployeeDao {
 		return false;
 	}
 
-	public Object selectById() {
+	public Employee selectById(int employeeId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Object selectByIdPassword() {
+	public Employee selectByIdPassword(int employeeId, String userpassword) {
 		// TODO Auto-generated method stub
 		return null;
 	}
